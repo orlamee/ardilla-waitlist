2499d86017089405b132d199bcd2be5f6ad8d6a9
orlamee <meetmichaelakindele@gmail.com>
2023-02-06 13:01:03 +0100

    fixes

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -6,7 +6,7 @@ function Banner() {
       <div className="container">
         <div className="row justify-content-center">
           <div className="col-md-6 text-center">
-            <h2>Your portals to more</h2>
+            <h2>Your portal to more</h2>
             <img src={liner} alt="liner" className="img-fluid" style={{marginLeft: "55px", marginTop: "-30px"}} />
             <h6 className="my-5">Wealth building is possible. You just need the right partner. With better financial tools, Ardilla has made it so much easier for you to start building wealth. Take advantage of the Ardilla platform by signing up with your email address.</h6>
             <div className="row height d-flex justify-content-center align-items-center">
